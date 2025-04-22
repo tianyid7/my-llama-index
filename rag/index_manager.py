a91568ca3c1f95192fd3f314610528ff78ae321c
Tianyi Deng <tdeng@blizzard.com>
2025-04-22 11:31:10 -0700

    :art: reorg

--- rag/index_manager.py
+++ rag/index_manager.py
@@ -1,6 +1,3 @@
-"""Main state management class for indices and prompts for
-experimentation UI"""
-
 import logging
 
 from llama_index.core import Settings, StorageContext, VectorStoreIndex
@@ -9,48 +6,72 @@ from llama_index.storage.index_store.redis import RedisIndexStore
 from llama_index.vector_stores.postgres import PGVectorStore
 from sqlalchemy import make_url
 
-logging.basicConfig(level=logging.INFO)  # Set the desired logging level
+logging.basicConfig(
+    level=logging.INFO
+)  # Set the desired logging level #TODO Make this configurable
 logger = logging.getLogger(__name__)
 
 
 class IndexManager:
     """
-    This class manages state for vector indexes,
-    docstores, query engines and chat engines
-    across the app's lifecycle (e.g. through UI manipulations).
-    The index_manager (instantiated) will be injected into all API calls
-    that need to access its state or manipulate its state.
-    This includes:
-    - Switching out vector indices or docstores
-    - Changing retrieval parameters (e.g. temperature, llm model, etc.)
+    This class manages index stores for
+    - vector (pgvector)
+    - indexes (redis)
+    - docs (redis)
     """
 
     def __init__(
         self,
         pgvector_conn: str,
+        pgvector_table: str,
         redis_host: str,
     ):
         # Stores
         self.pgvector_conn = pgvector_conn
+        self.pgvector_table = pgvector_table
         self.redis_host = redis_host
 
         self.embed_model = Settings.embed_model
 
-        self.base_index = self.get_vector_index(
+        self.base_index = self._create_vector_index(
             pgvector_conn=self.pgvector_conn,
+            pgvector_table=self.pgvector_table,
             redis_host=self.redis_host,
         )
 
         self.query_engine = None
 
-    def get_vector_index(
+    def get_current_index_info(self) -> dict:
+        """Return the indices currently being used"""
+        return {
+            "vector_store": {
+                "type": "PGVectorStore",
+                "connection": self.pgvector_conn,
+                "table": self.pgvector_table,
+            },
+            "docstore": {
+                "type": "RedisDocumentStore",
+                "host": self.redis_host,
+                "port": 6379,
+            },
+            "index_store": {
+                "type": "RedisIndexStore",
+                "host": self.redis_host,
+                "port": 6379,
+            },
+            "embed_model": self.embed_model.model_name,
+            "summary": self.base_index.summary,
+        }
+
+    def _create_vector_index(
         self,
         pgvector_conn: str,
+        pgvector_table: str,
         redis_host: str,
         redis_port: int = 6379,
     ) -> VectorStoreIndex:
         """
-        Returns a llamaindex VectorStoreIndex object which contains a storage context
+        Returns a VectorStoreIndex object which contains a storage context
         """
         # Create the vector store
         url = make_url(pgvector_conn)
@@ -60,8 +81,8 @@ class IndexManager:
             password=url.password,
             port=url.port,
             user=url.username,
-            table_name="paul_graham_essay",
-            embed_dim=1536,  # openai embedding dimension
+            table_name=pgvector_table,
+            embed_dim=1536,
             hnsw_kwargs={
                 "hnsw_m": 16,
                 "hnsw_ef_construction": 64,
@@ -70,7 +91,7 @@ class IndexManager:
             },
         )
 
-        # Create the document store TODO: doc_store and index_store might better be the same with vector_store
+        # Create the document store
         docstore = RedisDocumentStore.from_host_and_port(
             host=redis_host,
             port=redis_port,
@@ -92,34 +113,3 @@ class IndexManager:
             nodes=[], storage_context=storage_context, embed_model=self.embed_model
         )
         return vector_store_index
-
-    def get_current_index_info(self) -> dict:
-        """Return the indices currently being used"""
-        return {
-            "vector_store": {
-                "type": "PGVectorStore",
-                "connection": self.pgvector_conn,
-            },
-            "docstore": {
-                "type": "RedisDocumentStore",
-                "host": self.redis_host,
-                "port": 6379,
-            },
-            "index_store": {
-                "type": "RedisIndexStore",
-                "host": self.redis_host,
-                "port": 6379,
-            },
-            "embed_model": self.embed_model.model_name,
-            "summary": self.base_index.summary,
-        }
-
-    def set_current_index(
-        self, pgvector_conn: str, redis_host: str, redis_port: int = 6379
-    ) -> None:
-        """Set the current indices to be used for the RAG"""
-        self.base_index = self.get_vector_index(
-            pgvector_conn,
-            redis_host,
-            redis_port,
-        )
