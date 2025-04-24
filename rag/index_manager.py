eb1d74320708cd1576e7dd7f8faecc13b148e264
Tianyi Deng <tdeng@blizzard.com>
2025-04-23 20:31:36 -0700

    :recycle: make create_storage_context function instead of method

--- rag/index_manager.py
+++ rag/index_manager.py
@@ -22,13 +22,62 @@ logging.basicConfig(level=logging.INFO)  # Set the desired logging level
 logger = logging.getLogger(__name__)
 
 
+def create_storage_context() -> StorageContext:
+    """
+    Create a storage context with the vector store (pgvector), document store (pgvector), and index store (redis).
+    """
+
+    # Create the vector store
+    if not PGVECTOR_CONN_STR or not PGVECTOR_TABLE:
+        raise ValueError("PGVECTOR_CONN_STR and PGVECTOR_TABLE must be set.")
+
+    url = make_url(PGVECTOR_CONN_STR)
+    vector_store = PGVectorStore.from_params(
+        database=url.database,
+        host=url.host,
+        password=url.password,
+        port=url.port,
+        user=url.username,
+        table_name=PGVECTOR_TABLE,
+        embed_dim=EMBED_DIM,
+        hybrid_search=HYBRID_SEARCH_ENABLED,
+        hnsw_kwargs={
+            "hnsw_m": 16,
+            "hnsw_ef_construction": 64,
+            "hnsw_ef_search": 40,
+            "hnsw_dist_method": "vector_cosine_ops",
+        },
+    )
+
+    if not REDIS_PORT or not REDIS_HOST:
+        raise ValueError("REDIS_PORT and REDIS_HOST must be set.")
+
+    # Create the document store
+    docstore = RedisDocumentStore.from_host_and_port(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        namespace=DOC_STORE_NAMESPACE,
+    )
+
+    # Create the index store
+    index_store = RedisIndexStore.from_host_and_port(
+        host=REDIS_HOST, port=REDIS_PORT, namespace=INDEX_STORE_NAMESPACE
+    )
+
+    return StorageContext.from_defaults(
+        vector_store=vector_store,
+        docstore=docstore,
+        index_store=index_store,
+    )
+
+
 class IndexManager:
     """
     This class manages vector index stores
     """
 
     def __init__(self):
-        self.storage_context = self._create_storage_context()
+        self.storage_context = create_storage_context()
 
         self.embed_model = Settings.embed_model
 
@@ -43,54 +92,6 @@ class IndexManager:
         logger.info(f"Created a base vector store index: {vector_store_index.summary}")
 
         return vector_store_index
-
-    def _create_storage_context(self) -> StorageContext:
-        """
-        Create a storage context with the vector store (pgvector), document store (pgvector), and index store (redis).
-        """
-
-        # Create the vector store
-        if not PGVECTOR_CONN_STR or not PGVECTOR_TABLE:
-            raise ValueError("PGVECTOR_CONN_STR and PGVECTOR_TABLE must be set.")
-
-        url = make_url(PGVECTOR_CONN_STR)
-        vector_store = PGVectorStore.from_params(
-            database=url.database,
-            host=url.host,
-            password=url.password,
-            port=url.port,
-            user=url.username,
-            table_name=PGVECTOR_TABLE,
-            embed_dim=EMBED_DIM,
-            hybrid_search=HYBRID_SEARCH_ENABLED,
-            hnsw_kwargs={
-                "hnsw_m": 16,
-                "hnsw_ef_construction": 64,
-                "hnsw_ef_search": 40,
-                "hnsw_dist_method": "vector_cosine_ops",
-            },
-        )
-
-        if not REDIS_PORT or not REDIS_HOST:
-            raise ValueError("REDIS_PORT and REDIS_HOST must be set.")
-
-        # Create the document store
-        docstore = RedisDocumentStore.from_host_and_port(
-            host=REDIS_HOST,
-            port=REDIS_PORT,
-            namespace=DOC_STORE_NAMESPACE,
-        )
-
-        # Create the index store
-        index_store = RedisIndexStore.from_host_and_port(
-            host=REDIS_HOST, port=REDIS_PORT, namespace=INDEX_STORE_NAMESPACE
-        )
-
-        return StorageContext.from_defaults(
-            vector_store=vector_store,
-            docstore=docstore,
-            index_store=index_store,
-        )
 
     def create_vector_index_with_docs(
         self, documents: List[Document]
