0bdcae58e8505b1a2cfa104782b23ff34956a83d
Tianyi Deng <tdeng@blizzard.com>
2025-04-25 15:58:38 -0700

    :sparkles: added log_function_time decorator

--- common/decorators.py
+++ common/decorators.py
@@ -1,3 +1,7 @@
+import time
+from functools import wraps
+
+
 def singleton(cls):
     instances = {}
 
@@ -7,3 +11,16 @@ def singleton(cls):
         return instances[cls]
 
     return get_instance
+
+
+def log_function_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        print(f"Function '{func.__name__}' executed in {execution_time:.4f} seconds")
+        return result
+
+    return wrapper
